850500a8c594d0768b6a6b09c14e80a8866f38d6
Leandro Schoonewolff <wbbeyblade@gmail.com>
2025-03-06 17:39:34 -0500

    Modification dans CdDTO: Ajout des paramètres oubliés à CdDTO(dateParution et nbExemplaires)

--- src/main/java/ca/cal/tp2/service/dto/CdDTO.java
+++ src/main/java/ca/cal/tp2/service/dto/CdDTO.java
@@ -3,9 +3,11 @@ package ca.cal.tp2.service.dto;
 
 import ca.cal.tp2.modele.CD;
 
-public record CdDTO(int id,String titre, String artiste, String genre,int duree ) {
+import java.time.LocalDate;
+
+public record CdDTO(int id, String titre, LocalDate dateParution, int nbExemplaires,String artiste, String genre, int duree ) {
     public static CdDTO toDto(CD cd) {
-        return new CdDTO(cd.getId(),cd.getTitre(),cd.getArtiste(), cd.getGenre(), cd.getDuree());
+        return new CdDTO(cd.getId(),cd.getTitre(), cd.getDateParution(),cd.getNbExemplaires(),cd.getArtiste(), cd.getGenre(), cd.getDuree());
     }
 }
 
